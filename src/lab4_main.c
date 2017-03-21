1b2f421653a89bb29c3f33be19afbbfcf7a69946
Zachary Rump <zrrm74@mail.missouri.edu>
2017-03-21 11:50:27 -0500

    Continue work..need to finish part 3 dynamic threads

--- src/lab4_main.c
+++ src/lab4_main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include <pthread.h>
 #include "serial_ece4220.h"
 
 #define DATA_MAX 256
+#define FIFO_ID 1
 
 typedef struct gps_st {
 	uint8_t data;
@@ -16,15 +18,38 @@ typedef struct gps_st {
 pthread_t tid;
 
 // Common buffer for data and time
+int i=0;
 gps_t data_buffer[DATA_MAX];
+
+void read_fifo(void *args) {
+	struct timeval tv_buf;
+	int fd = open("/dev/rtf/1", O_RDWR);
+	if(-1 == fd)
+	{
+		return;
+	}
+	while(1) {
+		int count = read(fd, &tv_buf, sizeof(tv_buf));
+		if(count < 0)
+		{
+			return;
+		}
+		else
+		{
+			// spin up thread on each button press to wait for the next data
+			printf("Button pressed\n");
+			printf("Kernel Event Time: %u.%ld \n", (unsigned int)tv_buf.tv_sec, tv_buf.tv_usec);
+			printf("Last GPS Event: Data: %d, Time: %u.%ld \n", data_buffer[i-1].data, (unsigned int)data_buffer[i-1].tv.tv_sec, data_buffer[i-1].tv.tv_usec);
+		}
+	}
+}
 
 int main(int argc, char **argv) {
 	// Create thread to read from fifo (kernel)
-	// pthread_create(&tid, NULL, (void *)read_fifo, NULL); //(void *)
+	pthread_create(&tid, NULL, (void *)read_fifo, NULL); //(void *)
 	// Attempt to open serial port
 	int port_id = serial_open(0, 0, 5);
         unsigned char buf=0;
-	int i=0;
 	ssize_t num_bytes=0;
 	// Endlessly loop and read from serial port
 	while(1)
@@ -47,8 +72,8 @@ int main(int argc, char **argv) {
 				// Error gettime()
 				break; 
 			}
-			printf("Data: %d, Time: %u.%ld \n", data_buffer[i].data, (unsigned int)data_buffer[i].tv.tv_sec, data_buffer[i].tv.tv_usec);
-			fflush(stdout);
+			//printf("Data: %d, Time: %u.%ld \n", data_buffer[i].data, (unsigned int)data_buffer[i].tv.tv_sec, data_buffer[i].tv.tv_usec);
+			//fflush(stdout);
 		}
 	}
 	serial_close(port_id);
